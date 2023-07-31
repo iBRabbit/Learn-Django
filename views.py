7480c78972d6991428962b01ab83dcb4753556f6
Felix Prima <felix.prima@binus.ac.id>
2023-07-31 23:18:01 +0700

    Templates Init

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("This is the about page.")
