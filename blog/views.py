f3b3a71d49c6268d42c1c0ee77afc389677058d8
Felix Prima <felix.prima@binus.ac.id>
2023-08-01 00:09:55 +0700

    Templates pada App

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return render(request, 'blog.html')
+    return render(request, 'blog/index.html')
 
 def recent_post(request):
     return HttpResponse("This is the recent post page.")
